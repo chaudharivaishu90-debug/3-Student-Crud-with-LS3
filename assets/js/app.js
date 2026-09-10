a12243942f2dc5f6354d52d6ac5c6f5eb498a234
chaudharivaishu90-debug <chaudharivaishu90@gmail.com>
2026-09-10 12:39:00 +0530

    Student Crud With LS4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,106 +1,98 @@
-var cl=console.log;
-const Userdata=document.getElementById("Userdata");
-const Form=document.getElementById("Form");
-const name=document.getElementById("name");
-const Email=document.getElementById("Email");
-const Contact=document.getElementById("Contact");
-const submit=document.getElementById("submit");
-const update=document.getElementById("update");
+// var cl=console.log;
+// const MovieData=document.getElementById("MovieData");
+// const form=document.getElementById("form");
+// const nameInput=document.getElementById("nameInput");
+// const review=document.getElementById("review");
+// const rating=document.getElementById("rating");
+// const submit=document.getElementById("submit");
+// const update=document.getElementById("update");
 
 
-// let contactArr=[{id:"112",name:"Bhargav",email:"Bhargav@gmail.com",contact:"9878788543"}];
+// let MoveNmaearr=[{id:"111",movieName:"3 Idiot",Review:"Good",Rating:"5"}];
 
-// localStorage.setItem("contactArr",JSON.stringify(contactArr));
+//  localStorage.setItem("MoveNmaearr",JSON.stringify(MoveNmaearr));
 
-let contactArr=JSON.parse(localStorage.getItem("contactArr")) || [{id:"112",name:"Bhargav",email:"Bhargav@gmail.com",contact:"9878788543"}];
-cl(contactArr);
+let MoveNmaearr=JSON.parse(localStorage.getItem("MoveNmaearr"))||[];
 
-function showtable(eve){
+
+
+//database
+function ShowUi(para) {
 	let result=``;
-	eve.forEach((element,i) => {
-		result+=`<tr id="${element.id}">
-                                <td>${i+1}</td>
-                                <td>${element.name}</td>
-                                <td>${element.email}</td>
-                                    <td>${element.contact}</td>
-
-
-                                <td>                   
-                                 <i  onclick="OnEdt(this)" class="fa-solid fa-pen-to-square fa-2x text-primary"></i></td>
-                                <td><i  onclick="Ondlt(this)" class="fa-regular fa-trash-can fa-2x text-danger"></i></td>
-                            </tr>
+	para.forEach((ele,i)=> {
+		result+=` <tr id="${ele.id}">
+                            <td>${i+1}</td>
+                            <td>${ele.movieName}</td>
+                            <td>${ele.Review}</td>
+                            <td>${ele.Rating}</td>
+                            <td> <i  onclick="onEdittr(this)" class="fa-solid fa-pen-to-square fa-2x text-primary"></i></td>
+                            <td><i onclick="onRemove(this)" class="fa-regular fa-trash-can fa-2x text-danger"></i></td>
+                        </tr>
 		`
+		MovieData.innerHTML=result;
 	});
-    		Userdata.innerHTML=result;
-
+	
 }
-showtable(contactArr)
+ShowUi(MoveNmaearr);
 
 //create
-function CreateTable(ele){
-	
-	let tr=document.createElement("tr");
-		tr.id=ele.id;
-
-	tr.innerHTML+=`<td>${contactArr.length}</td>
-                                <td>${ele.name}</td>
-                                <td>${ele.email}</td>
-                                    <td>${ele.contact}</td>
-
-
-                                <td>                   
-                                 <i onclick="OnEdt(this)" class="fa-solid fa-pen-to-square fa-2x text-primary"></i></td>
-                                <td><i onclick="Ondlt(this)" class="fa-regular fa-trash-can fa-2x text-danger"></i></td>
-`
-Userdata.append(tr);
-Swal.fire({
+function CreateMovie(obj){
+let tr=document.createElement("tr");
+tr.id=obj.id;
+tr.innerHTML=`
+                            <td>${MoveNmaearr.length}</td>
+                            <td>${obj.movieName}</td>
+                            <td>${obj.Review}</td>
+                            <td>${obj.Rating}</td>
+                            <td> <i  onclick="onEdittr(this)" class="fa-solid fa-pen-to-square fa-2x text-primary"></i></td>
+                            <td><i onclick="onRemove(this)"class="fa-regular fa-trash-can fa-2x text-danger"></i></td>
+    
+		`;
+		MovieData.append(tr);
+		Swal.fire({
 			title:" data Created",
 			timer:2000,
 			icon:"success"
 		});
-
-
 }
 
+
+
+
 //Edit
-function OnEdt(ele){
-	let EditID= ele.closest("tr").id;
-	EDobj=contactArr.find(e=>e.id===EditID);
-	name.value=EDobj.name
-		Email.value=EDobj.email
-			Contact.value=EDobj.contact
-	submit.classList.add("d-none")
-	update.classList.remove("d-none")
-	localStorage.setItem("EditID",EditID);
-
-
+function onEdittr(e){
+	let Editid= e.closest("tr").id;
+	let objfind=MoveNmaearr.find(t=>t.id===Editid);
+	nameInput.value=objfind.movieName;
+	review.value=objfind.Review;
+	rating.value=objfind.Rating;
+	submit.classList.add("d-none");
+	update.classList.remove("d-none");
+	localStorage.setItem("Editid",Editid);
 
 }
 
 //update
-	
-function oupHandler(event){
-	let UpdateId= localStorage.getItem("EditID");
-		localStorage.removeItem("EditID");
-		let index= contactArr.findIndex(t=>t.id===UpdateId);
+function onupdateBtn(event){
+	let UpdateId= localStorage.getItem("Editid")
+		localStorage.removeItem("Editid");
+		let index= MoveNmaearr.findIndex(t=>t.id===UpdateId);
 		
-	contactArr[index]={
-        id:UpdateId,
-		name:name.value,
-        email:Email.value,
-        contact:Contact.value
-
-		}
-let td=document.getElementById(UpdateId).children
-td[1].innerHTML=name.value,
-td[2].innerHTML=Email.value,
-td[3].innerHTML=Contact.value,
-
+	MoveNmaearr[index]={
+		id:UpdateId,
+		movieName:nameInput.value,
+		Review:review.value,
+		Rating:rating.value
+}
+let td=document.getElementById(UpdateId).children;
+td[1].innerHTML=nameInput.value;
+td[2].innerHTML=review.value;
+td[3].innerHTML=rating.value;
 submit.classList.remove("d-none");
 update.classList.add("d-none");
- localStorage.setItem("contactArr",JSON.stringify(contactArr));
+	localStorage.setItem("MoveNmaearr",JSON.stringify(MoveNmaearr));
 
-Form.reset();
+form.reset();
 Swal.fire({
 			title:" data updated",
 			timer:2000,
@@ -110,45 +102,27 @@ Swal.fire({
 }
 
 
-function Ondlt(ee){
-	let getconfirm=confirm("Want to remove??")
-	if(getconfirm){
-let removeId=ee.closest("tr").id;
-	let index=contactArr.findIndex(t=>t.id===removeId);
-	contactArr.splice(index,1);
-		ee.closest("tr").remove();
-		let td=[...document.querySelectorAll("#Userdata tr td:first-child")]
-		td.forEach((element,i )=> {
-			element.innerHTML=i+1;
-             localStorage.setItem("contactArr",JSON.stringify(contactArr));
+//delete
+function onRemove(ee){
+	let confirmation=confirm("Are sure To delete??")
+	if(confirmation){
+	let removeID=ee.closest("tr").id;
+	let index=MoveNmaearr.findIndex(t=>t.id===removeID);
+	MoveNmaearr.splice(index,1);
+	
+	ee.closest("tr").remove();
+	let td=[...document.querySelectorAll("#MovieData tr td:first-child")];
+	td.forEach((ele,i)=>{
+		ele.innerHTML=i+1;
+			localStorage.setItem("MoveNmaearr",JSON.stringify(MoveNmaearr));
 
-
-		});
-		Swal.fire({
-			title:" data Delete",
+	});
+	}
+Swal.fire({
+			title:" data Deleted",
 			timer:2000,
 			icon:"success"
-		});
-
-	}
-	}
-
-
-function Onhandler(e){
-	e.preventDefault();
-	let obj={
-		id:crypto.randomUUID(),
-		name:name.value,
-        email:Email.value,
-        contact:Contact.value
-
-	}
-	contactArr.push(obj)
-	CreateTable(obj);
-    localStorage.setItem("contactArr",JSON.stringify(contactArr));
-
-	Form.reset();
-
+		})
 
 }
 
@@ -156,5 +130,21 @@ function Onhandler(e){
 
 
 
-Form.addEventListener("submit",Onhandler);
-update.addEventListener("click",oupHandler);
+function onclickhanlder(p){
+	p.preventDefault();
+	let obj={
+		id:crypto.randomUUID(),
+		movieName:nameInput.value,
+		Review:review.value,
+		Rating:rating.value
+	}
+	MoveNmaearr.push(obj);
+	CreateMovie(obj);
+	localStorage.setItem("MoveNmaearr",JSON.stringify(MoveNmaearr));
+
+
+	form.reset();
+}
+
+form.addEventListener("submit",onclickhanlder);
+update.addEventListener("click",onupdateBtn);
